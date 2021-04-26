7854c85cb970f8a0e5d0754e408be4b59505efe8
chan <chan@semantia.com.au>
2021-04-26 18:57:09 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 
 import { Switch, Route } from 'react-router-dom';
-
+import Header from './components/header/header.component.jsx';
 const HatPage =() =>(
   <div>
     <h1>HATS PAGE</h1>
@@ -13,6 +13,7 @@ const HatPage =() =>(
 function App() {
   return (
     <div>
+      <Header/>
           <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route  path='/shop' component={ShopPage}/>
