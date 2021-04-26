c4a68337eb6e9ac45e2a5a9ad8248276cd1bb9d9
chan <chan@semantia.com.au>
2021-04-26 17:49:17 +0800

    added route to navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom';
+
+const HatPage =() =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+          <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route  path='/hats' component={HatPage}/>
+          </Switch>
     </div>
   );
 }
