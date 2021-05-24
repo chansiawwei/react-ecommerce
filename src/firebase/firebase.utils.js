6fe0afd1e3f80145736f60616e8336db9d524289
Chan Siaw Wei <siawwei98@gmail.com>
2021-05-24 12:32:50 +0800

    implemented signup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,29 @@ var firebaseConfig = {
     appId: "1:943466961897:web:c500ec635952e1f5af0978",
     measurementId: "G-0N44JN2MJQ"
   };
-
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+  
   firebase.initializeApp(firebaseConfig);
 
   export const auth = firebase.auth();
