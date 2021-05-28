6448bb9d4b11d3b2d18b69aec3e73c6fda651572
Chan Siaw Wei <siawwei98@gmail.com>
2021-05-28 13:14:03 +0800

    Added cart dropdown with toggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from "../../firebase/firebase.utils";
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
 
 <div className="header">
         <Link className="logo-container" to="/">
@@ -20,11 +22,26 @@ const Header = ({currentUser}) => (
                 :
                 <Link className="option" to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null :  <CartDropdown/>
+            
+        }
+        
     </div>
 )
+// const mapStateToProps = state =>({  
+//     currentUser:state.user.currentUser,
+//     hidden:state.cart.hidden
+// })
 
-const mapStateToProps = state =>({
-    currentUser:state.user.currentUser
+/**
+ *  This is just another way of destructe data
+ *  Refer top codes for original version 
+ */
+const mapStateToProps = ({user:{currentUser} , cart : {hidden}}) =>({  
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header)
